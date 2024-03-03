da0396ac43181145d438429aab1026f55ec88738
mimi99 <0mini9939@gmail.com>
2024-03-03 22:17:02 +0900

    feat: training path to query

--- src/api/trainingApi.ts
+++ src/api/trainingApi.ts
@@ -2,14 +2,14 @@ import axios from "axios";
 import { Constants } from "../constant/constant";
 import { ArticleInfoType } from "../types/ArticleInfoType";
 
-const methodUrl = "api/trainings/";
+const methodUrl = "api/trainings";
 
 export const getTrainings = async (
-  year: string
+  year?: string
 ): Promise<ArticleInfoType[]> => {
   return await axios
     .get<{ trainingLogs: ArticleInfoType[] }>(
-      Constants.BASE_URL + methodUrl + year
+      `${Constants.BASE_URL}${methodUrl}?year=${year ?? ""}`
     )
     .then((response) => response.data.trainingLogs);
 };
