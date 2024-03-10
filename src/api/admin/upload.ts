9fa8464d335c71cd9c1653fb19ed593a7186eb3d
mimi99 <0mini9939@gmail.com>
2024-03-10 20:58:59 +0900

    feat: update, delete

--- src/api/admin/upload.ts
+++ src/api/admin/upload.ts
@@ -39,3 +39,63 @@ export const uploadBoard = async (
     return false;
   }
 };
+
+export const updateBoard = async (
+  articleInfo: ArticleInfoType,
+  boardType: "news" | "training" | "notice"
+) => {
+  const cookies = new Cookies();
+  try {
+    const res = await axios.post(
+      `${Constants.BASE_URL}${methodUrl}`,
+      {
+        id: articleInfo.id,
+        title: articleInfo.title,
+        author: articleInfo.author,
+        boardType,
+        dateTime: articleInfo.dateTime,
+        description: articleInfo.description,
+        tags: articleInfo.tags,
+        base64Imgs: articleInfo.imgSrcs,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${cookies.get("JSESSIONID")}`,
+        },
+        withCredentials: true,
+      }
+    );
+    if (res) {
+      return true;
+    }
+    return false;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+};
+
+export const deleteBoard = async (
+  id: string,
+  boardType: "news" | "training" | "notice"
+) => {
+  const cookies = new Cookies();
+  try {
+    const res = await axios.delete(
+      `${Constants.BASE_URL}${methodUrl}?id=${id}&board=${boardType}`,
+      {
+        headers: {
+          Authorization: `Bearer ${cookies.get("JSESSIONID")}`,
+        },
+        withCredentials: true,
+      }
+    );
+    if (res) {
+      return true;
+    }
+    return false;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+};
