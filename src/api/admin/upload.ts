2e23115a56b6d8ccecb58fe3a75cc52cc8f04e14
mimi99 <0mini9939@gmail.com>
2024-03-11 20:59:31 +0900

    feat: crud form

--- src/api/admin/upload.ts
+++ src/api/admin/upload.ts
@@ -5,6 +5,23 @@ import { Cookies } from "react-cookie";
 
 const methodUrl = "api/admin/upload/";
 
+/**
+ * Create board
+ * @description
+ * ```bash
+ * curl BASE_URL/api/admin/upload/
+ * -X POST 
+ * -d '{
+        title: articleInfo.title,
+        author: articleInfo.author,
+        boardType,
+        dateTime: articleInfo.dateTime,
+        description: articleInfo.description,
+        tags: articleInfo.tags,
+        base64Imgs: articleInfo.imgSrcs,
+      }'
+  ```
+ */
 export const uploadBoard = async (
   articleInfo: Omit<ArticleInfoType, "id">,
   boardType: "news" | "training" | "notice"
@@ -40,13 +57,31 @@ export const uploadBoard = async (
   }
 };
 
+/**
+ * Update board 
+ * @description
+ * ```bash
+ * curl BASE_URL/api/admin/upload/
+ * -X PATCH 
+ * -d '{
+ *      id: articleInfo.id,
+        title: articleInfo.title,
+        author: articleInfo.author,
+        boardType,
+        dateTime: articleInfo.dateTime,
+        description: articleInfo.description,
+        tags: articleInfo.tags,
+        base64Imgs: articleInfo.imgSrcs,
+      }'
+  ```
+ */
 export const updateBoard = async (
   articleInfo: ArticleInfoType,
   boardType: "news" | "training" | "notice"
 ) => {
   const cookies = new Cookies();
   try {
-    const res = await axios.post(
+    const res = await axios.patch(
       `${Constants.BASE_URL}${methodUrl}`,
       {
         id: articleInfo.id,
@@ -75,6 +110,15 @@ export const updateBoard = async (
   }
 };
 
+/**
+ * Delete board
+ * @description
+ * ```bash
+ * curl BASE_URL/api/admin/upload/
+ * -X DELETE 
+ * --data "id=id&board=boardType"
+  ```
+ */
 export const deleteBoard = async (
   id: string,
   boardType: "news" | "training" | "notice"
