a2844f95548efa126883e9750b3d20ec3eeae222
mimi99 <0mini9939@gmail.com>
2024-03-18 23:18:22 +0900

    chore: ga

--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,8 +1,6 @@
-import useGoogleAnalytics from "../ga/googleAnalytics";
 import SitemapRoutes from "../seo/sitemapRoutes";
 
 function AppRouter() {
-  useGoogleAnalytics();
   return <>{SitemapRoutes}</>;
 }
 
