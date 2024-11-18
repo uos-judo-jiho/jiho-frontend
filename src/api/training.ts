66bfb53cb145548b6a5ed6f56d4a8ba8f3bab07a
99mini <43674669+99mini@users.noreply.github.com>
2024-11-18 15:40:38 +0900

    fix: api query (#64)
    * refactor: update getTrainings API to use params for year

--- src/api/training.ts
+++ src/api/training.ts
@@ -3,11 +3,10 @@ import axiosInstance from "./config";
 
 const METHOD_URL = "api/trainings";
 
-export const getTrainings = async (
-  year?: string
-): Promise<ArticleInfoType[]> => {
+export const getTrainings = async (year?: string): Promise<ArticleInfoType[]> => {
   return await axiosInstance<{ trainingLogs: ArticleInfoType[] }>({
-    url: `${METHOD_URL}?year=${year ?? ""}`,
+    url: `${METHOD_URL}`,
+    params: { year },
     method: "GET",
     withCredentials: true,
   }).then((response) => response.data.trainingLogs);
