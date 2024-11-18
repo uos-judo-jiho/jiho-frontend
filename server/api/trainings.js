c45ddd5cf435f755f0ee3727c7c4718780558f5f
99mini <itman390@gmail.com>
2024-11-18 15:39:11 +0900

    refactor: update getTrainings API to use params for year

--- server/api/trainings.js
+++ server/api/trainings.js
@@ -4,7 +4,10 @@ const METHOD_URL = "api/trainings";
 
 export const getTrainings = async (year) => {
   return await axiosInstance({
-    url: `${METHOD_URL}?year=${year ?? ""}`,
+    url: `${METHOD_URL}`,
+    params: {
+      year,
+    },
     method: "GET",
     withCredentials: true,
   }).then((response) => response.data.trainingLogs);
