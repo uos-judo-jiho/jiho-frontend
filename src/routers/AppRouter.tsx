2a8d6d847f696ae0020db879f448ebc2a2b3ade1
99mini <itman390@gmail.com>
2025-07-30 11:29:21 +0900

    feat: with suspense

--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,7 +1,13 @@
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 
+import Loading from "@/components/common/Skeletons/Loading";
+
 import Home from "../pages/Home";
+
+const WithSuspense = ({ children }: { children: React.ReactNode }) => {
+  return <Suspense fallback={<Loading />}>{children}</Suspense>;
+};
 
 const About = lazy(() => import("../pages/About"));
 const NewsPage = lazy(() => import("../pages/News"));
@@ -18,17 +24,94 @@ const AdminHomePage = lazy(() => import("../pages/admin/AdminHomePage"));
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/news" element={<News />} />
-      <Route path="/news/:id" element={<NewsDetail />} />
-      <Route path="/news/:id/:index" element={<NewsPage />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/photo" element={<PhotoPC />} />
-      <Route path="/photo/:id" element={<PhotoPage />} />
-      <Route path="/notice" element={<Notice />} />
-      <Route path="/notice/:id" element={<NoticeDetail />} />
-      <Route path="*" element={<NotFound />} />
-      <Route path="/admin/*" element={<AdminHomePage />} />
+      <Route
+        path="/"
+        element={
+          <WithSuspense>
+            <Home />
+          </WithSuspense>
+        }
+      />
+      <Route
+        path="/news"
+        element={
+          <WithSuspense>
+            <News />
+          </WithSuspense>
+        }
+      />
+      <Route
+        path="/news/:id"
+        element={
+          <WithSuspense>
+            <NewsDetail />
+          </WithSuspense>
+        }
+      />
+      <Route
+        path="/news/:id/:index"
+        element={
+          <WithSuspense>
+            <NewsPage />
+          </WithSuspense>
+        }
+      />
+      <Route
+        path="/about"
+        element={
+          <WithSuspense>
+            <About />
+          </WithSuspense>
+        }
+      />
+      <Route
+        path="/photo"
+        element={
+          <WithSuspense>
+            <PhotoPC />
+          </WithSuspense>
+        }
+      />
+      <Route
+        path="/photo/:id"
+        element={
+          <WithSuspense>
+            <PhotoPage />
+          </WithSuspense>
+        }
+      />
+      <Route
+        path="/notice"
+        element={
+          <WithSuspense>
+            <Notice />
+          </WithSuspense>
+        }
+      />
+      <Route
+        path="/notice/:id"
+        element={
+          <WithSuspense>
+            <NoticeDetail />
+          </WithSuspense>
+        }
+      />
+      <Route
+        path="*"
+        element={
+          <WithSuspense>
+            <NotFound />
+          </WithSuspense>
+        }
+      />
+      <Route
+        path="/admin/*"
+        element={
+          <WithSuspense>
+            <AdminHomePage />
+          </WithSuspense>
+        }
+      />
     </Routes>
   );
 };
