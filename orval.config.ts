f888aa5e0c87b5ba58165ccd6da0496c1de8ecf5
99mini <itman390@gmail.com>
2026-02-01 19:04:22 +0900

    chore: add orval commands to package.json and integrate into build process

---
+++ orval.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "orval";
+
+export default defineConfig({
+  jiho: {
+    input: {
+      target: "https://uosjudo.com/v3/api-docs",
+    },
+    output: {
+      target: "src/shared/api/_generated/index.ts",
+      schemas: "src/shared/api/_generated/model",
+      client: "react-query",
+      httpClient: "axios",
+      clean: true,
+      baseUrl: "https://uosjudo.com",
+      override: {
+        query: {
+          version: 5,
+          useQuery: true,
+          useMutation: true,
+          useInfinite: true,
+          usePrefetch: true,
+          useInvalidate: true,
+          shouldExportHttpClient: false,
+          shouldExportQueryKey: true,
+          shouldSplitQueryKey: true,
+        },
+      },
+    },
+  },
+});
--- script/build.sh
+++ script/build.sh
@@ -2,6 +2,7 @@
 node script/sitemap.js
 
 npm run clean
+npm run orval
 npm run build:client
 npm run build:server
 npm run build:server-ts
