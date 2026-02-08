95300d7d794cbbb04bbda5b450a5352b47ad4d64
99mini <itman390@gmail.com>
2026-02-08 21:13:45 +0900

    feat: update HomePage to use v2 API for admin profile and refactor authentication logic

--- apps/admin/src/pages/HomePage.tsx
+++ apps/admin/src/pages/HomePage.tsx
@@ -2,20 +2,32 @@ import Router from "@/app/routers/Router";
 import Login from "@/components/admin/form/Login";
 import DefaultLayout from "@/components/layouts/DefaultLayout";
 import SheetWrapper from "@/components/layouts/SheetWrapper";
-import { useState } from "react";
+import { v2Admin } from "@packages/api";
 
 const HomePage = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const {
+    data: adminProfile,
+    isLoading,
+    isSuccess,
+    refetch,
+  } = v2Admin.useGetApiV2AdminMe({
+    query: {
+      retry: false,
+    },
+    axios: {
+      withCredentials: true,
+    },
+  });
 
   return (
     <DefaultLayout>
       {/* TODO: 로그인 기능 구현 */}
-      {isAuthenticated ? (
+      {isLoading ? null : isSuccess && adminProfile?.data ? (
         <SheetWrapper>
           <Router />
         </SheetWrapper>
       ) : (
-        <Login onSuccess={() => setIsAuthenticated(true)} />
+        <Login onSuccess={() => refetch()} />
       )}
     </DefaultLayout>
   );
